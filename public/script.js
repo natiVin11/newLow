b28669c87c2feb19dc5ce907ccdc21d6f5588d48
natiVin11 <77802978+natiVin11@users.noreply.github.com>
2025-04-06 11:55:43 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ let apartmentsData = [];  // נתונים של הדירות
 
 // פונקציה לטעינת הדירות
 function loadApartments() {
-    fetch('http://localhost:3102/get-apartments')  // כתובת מלאה עם פרוטוקול
+    fetch('/get-apartments')  // כתובת מלאה עם פרוטוקול
         .then(response => response.json())
         .then(data => {
             apartmentsData = data;
@@ -85,7 +85,7 @@ function updateApartmentStatus(building, apartment, status) {
     });
 
     // שמירה בשרת
-    fetch('http://localhost:3102/update-status', {  // כתובת מלאה עם פרוטוקול
+    fetch('/update-status', {  // כתובת מלאה עם פרוטוקול
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
